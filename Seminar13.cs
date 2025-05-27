2e407c5e39da491e35584508933d70cf2d0bf159
M-a-r-i-o-pixel <hutualexandrumario@gmail.com>
2025-05-27 11:11:19 +0300

    Update Seminar13.cs

--- Seminar13.cs
+++ Seminar13.cs
@@ -1,130 +1,95 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Windows.Forms;
-
-namespace VoronoiWinForms
+namespace Seminar13
 {
-    public class VoronoiDiagram
-    {
-        private List<PointF> sites;
-        private RectangleF bounds;
-
-        public VoronoiDiagram(List<PointF> sites, RectangleF bounds)
-        {
-            this.sites = sites;
-            this.bounds = bounds;
-        }
-
-        public List<PointF[]> Compute()
-        {
-            var cells = new List<PointF[]>();
-            foreach (var site in sites)
-            {
-                List<PointF> cell = new List<PointF>
-                {
-                    new PointF(bounds.Left, bounds.Top),
-                    new PointF(bounds.Right, bounds.Top),
-                    new PointF(bounds.Right, bounds.Bottom),
-                    new PointF(bounds.Left, bounds.Bottom)
-                };
-
-                foreach (var other in sites)
-                {
-                    if (other == site) continue;
-
-                    float midX = (site.X + other.X) / 2;
-                    float midY = (site.Y + other.Y) / 2;
-                    float dx = other.Y - site.Y;
-                    float dy = site.X - other.X;
-
-                    float length = (float)System.Math.Sqrt(dx * dx + dy * dy);
-                    dx /= length;
-                    dy /= length;
-
-                    float a = dx;
-                    float b = dy;
-                    float c = -(a * midX + b * midY);
-
-                    List<PointF> newCell = new List<PointF>();
-                    for (int i = 0; i < cell.Count; i++)
-                    {
-                        PointF current = cell[i];
-                        PointF next = cell[(i + 1) % cell.Count];
-
-                        bool insideCurrent = a * current.X + b * current.Y + c <= 0;
-                        bool insideNext = a * next.X + b * next.Y + c <= 0;
-
-                        if (insideCurrent)
-                            newCell.Add(current);
-
-                        if (insideCurrent != insideNext)
-                        {
-                            float dxLine = next.X - current.X;
-                            float dyLine = next.Y - current.Y;
-
-                            float denominator = a * dxLine + b * dyLine;
-                            if (denominator == 0) continue;
-
-                            float t = -(a * current.X + b * current.Y + c) / denominator;
-                            if (t >= 0 && t <= 1)
-                            {
-                                PointF intersection = new PointF(
-                                    current.X + t * dxLine,
-                                    current.Y + t * dyLine
-                                );
-                                newCell.Add(intersection);
-                            }
-                        }
-                    }
-
-                    cell = newCell;
-                }
-
-                cells.Add(cell.ToArray());
-            }
-
-            return cells;
-        }
-    }
     public partial class Form1 : Form
     {
         List<PointF> points;
+        const int radius = 3;
+        Dictionary<PointF, Brush> pointToColor;
+        bool voronoi;
 
+        Brush[] brushes = new Brush[] { Brushes.Red, Brushes.Green, Brushes.Blue, Brushes.Purple, Brushes.Yellow, Brushes.DarkBlue };
+        //the number of colors is 6 so the max number of points must be 6 
         public Form1()
         {
             InitializeComponent();
-            this.DoubleBuffered = true;
             points = new List<PointF>();
-            this.Paint += Form1_Paint;
-            this.MouseClick += Form1_MouseClick;
+            pointToColor = new Dictionary<PointF, Brush>();
+            voronoi = false;
         }
 
-        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        private void Form1_Click(object sender, EventArgs e)
         {
-            points.Add(e.Location);
+            PointF newPoint = this.PointToClient(MousePosition);
+            points.Add(newPoint);
+            if (points.Count > brushes.Length) {
+                points.RemoveAt(0);
+            }
+            pointToColor = new Dictionary<PointF, Brush>();
+            voronoi = false;
             Invalidate();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            if (points.Count < 2) return;
-
-            RectangleF bounds = new RectangleF(0, 0, ClientSize.Width, ClientSize.Height);
-            VoronoiDiagram voronoi = new VoronoiDiagram(points, bounds);
-            var cells = voronoi.Compute();
-
-            int i = 0;
-            foreach (var cell in cells)
+            Graphics g = e.Graphics;
+            #region plot points
             {
-                var color = Color.FromArgb(80, 100 + i * 20 % 155, 100 + i * 40 % 155, 255);
-                e.Graphics.FillPolygon(new SolidBrush(color), cell);
-                e.Graphics.DrawPolygon(Pens.Black, cell);
-                i++;
+                int n = points.Count;
+                for (int i = 0; i < n; i++)
+                {
+                    string text = (i + 1).ToString();
+                    PointF p = points[i];
+                    g.DrawString(text, new Font(FontFamily.GenericMonospace, 10), Brushes.Navy, p.X - radius, p.Y + radius);
+                    g.DrawEllipse(Pens.Navy, p.X - radius, p.Y - radius, 2 * radius, 2 * radius);
+                }
             }
+            #endregion
 
-            foreach (var p in points)
-                e.Graphics.FillEllipse(Brushes.Red, p.X - 3, p.Y - 3, 6, 6);
+            if (voronoi) {
+                foreach (var kv in pointToColor) {
+                    PointF p = kv.Key;
+                    Brush brush = kv.Value;
+                    if (brush == Brushes.White)
+                        continue;
+                    g.FillEllipse(brush,p.X-radius,p.Y-radius,2*radius,2*radius);
+                    
+                    
+                }
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < ClientSize.Height; i++) {
+                for (int j = 0; j < ClientSize.Width; j++) {
+                    int x = j;
+                    int y = i;
+                    if (x % 7 == 0) continue;
+                    if(y%7==0) continue;
+                    PointF current = new PointF(x,y);
+                    int minIndex = -1;
+                    float minDistance = float.MaxValue;
+                    for (int k = 0; k < points.Count; k++) {
+                        float distance = Distance(current, points[k]);
+                        if (distance < minDistance) { minDistance = distance;minIndex = k; }
+                    }
+                    PointF minPoint = points[minIndex];
+                    if (minDistance < 20) {
+                        pointToColor[current] = Brushes.White;
+                        continue;
+                    }
+                    Brush currentBrush = brushes[minIndex];
+                    pointToColor[current] = currentBrush;
+
+                }
+            }
+            voronoi = true;
+            Invalidate();
+        }
+        public static float Distance(PointF p1, PointF p2) {
+            return (float)Math.Sqrt(Math.Pow(p1.X-p2.X,2)+Math.Pow(p1.Y-p2.Y,2));
         }
     }
 }
